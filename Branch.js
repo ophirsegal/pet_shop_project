18cbad87a7bb3842bc5311bf52945284505d855e
lielpapi <135997764+lielpapi@users.noreply.github.com>
2023-08-25 16:41:07 +0300

    Delete Branch.js

--- Branch.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const branchSchema = new mongoose.Schema({
-    name: String,
-    lat: Number,
-    lon: Number
-});
-module.exports = mongoose.model('Branch', branchSchema);

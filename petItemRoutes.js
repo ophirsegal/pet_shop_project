a4cad70254f602b363fd85745ed4d4084f218822
lielpapi <135997764+lielpapi@users.noreply.github.com>
2023-08-25 15:14:36 +0300

    Delete petItemRoutes.js

--- petItemRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const petItemController = require('../controllers/petItemController');
-
-router.post('/add', petItemController.addPetItem);
-router.get('/all', petItemController.getPetItems);
-
-module.exports = router;

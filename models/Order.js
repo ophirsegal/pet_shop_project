907fd3c103ae827ac027499d76d9aafe3f4b7480
lielpapi <135997764+lielpapi@users.noreply.github.com>
2023-08-26 21:50:41 +0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,28 +1,22 @@
-// Importing the mongoose library for working with MongoDB
 const mongoose = require('mongoose');
 const OrderSchema = new mongoose.Schema({
     
-    // An array of ordered items, each referencing a "PetItem" document
     orderedItems: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'PetItem' // Referencing the "PetItem" model
     }],
-    // The date when the order was placed, defaulting to the current date and time
     orderDate: {
         type: Date,
         default: Date.now
     },
-    // The username of the user who placed the order, required field
     username: {
         type: String,
         required: true
     },
-    // Indicates whether the order is completed, defaults to false
     isOrdered: {
         type: Boolean,
         default: false
     },
-    // The total price of the order, required field, default is 0
     totalPrice: {
         type: Number,
         required: true,
